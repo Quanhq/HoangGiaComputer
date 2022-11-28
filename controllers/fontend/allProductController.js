36336cf4d83e4988a672522e477a4eb39275349e
dev10sinka <107916907+dev10sinka@users.noreply.github.com>
2022-11-28 11:43:23 +0700

    .

--- controllers/fontend/allProductController.js
+++ controllers/fontend/allProductController.js
@@ -7,12 +7,18 @@ const { ObjectID } = require("bson");
 const Product = mongoose.model("Product");
 
 router.get("/category/", (req, res) => {
-  res.render("main/client/all-product")
+  res.render("main/client/all-product");
 });
 
 router.get("/all-product-by-category", async (req, res) => {
-  let data = await Product.find({ categoryName: req.query.categoryName });
-  res.send(data);
+  let number = Number(req.query.page);
+  let totalProduct = await Product.find({
+    categoryName: req.query.categoryName,
+  });
+  let data = await Product.find({ categoryName: req.query.categoryName })
+    .limit(12)
+    .skip((number - 1) * 12);
+  res.send([data, totalProduct.length]);
 });
 
 module.exports = router;
